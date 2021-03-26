cb4e30a7feebd506a2ae2ce73c9d0731365054a8
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-03-26 12:42:26 -0400

    PathChecker integrated in demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+from path_checker import PathChecker
 from pathlib import Path
 from sys import argv, exit
 
@@ -21,16 +22,18 @@ if __name__ == "__main__":
 	# Input path checks
 	try:
 		input_path = Path(argv[1])
+		input_chekcer = PathChecker(input_path, [_PDF_EXTENSION])
+
 	except IndexError:
 		print("ERROR! The path to a " + _PDF_EXTENSION
 			+ " file must be provided as the first argument.")
 		exit()
 
-	if not input_path.exists():
+	if not input_chekcer.path_exists():
 		print("ERROR! " + str(input_path) + " does not exist.")
 		exit()
 
-	if input_path.suffixes != _PDF_EXTENSION_IN_LIST: # False if not a file
+	if not input_chekcer.extension_is_correct(): # False if not a file
 		print("ERROR! The first argument must be the path to a "
 			+ _PDF_EXTENSION + " file.")
 		exit()
@@ -38,11 +41,12 @@ if __name__ == "__main__":
 	# Output path checks
 	try:
 		output_path = Path(argv[2])
+		output_checker = PathChecker(output_path, [_TXT_EXTENSION])
 
-		if output_path.is_dir():
+		if output_checker.path_is_dir():
 			output_path = output_path/_make_default_output_file_name(input_path)
 
-		elif output_path.suffixes != _TXT_EXTENSION_IN_LIST:
+		elif not output_checker.extension_is_correct():
 			output_path = output_path.with_suffix(_TXT_EXTENSION)
 
 	except IndexError:
---
+++ path_checker.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+
+class PathChecker:
+
+	def __init__(self, a_path, suffixes):
+		self.path = a_path
+		self.extension = suffixes
+
+	def add_suffix(self, suffix):
+		self.extension.append(suffix)
+
+	@property
+	def extension(self):
+		"""
+		The extension that the checked path is supposed to have, stored as a
+		list of suffixes. For example, a PDF file's suffix list is ['.pdf']; a
+		Linux archive file's suffix list is ['.tar', '.gz']. Every extension
+		must start with a '.'. The suffixes can be given in a tuple. If this
+		property is set to None, it will be and empty list.
+		"""
+		return self._extension
+
+	@extension.setter
+	def extension(self, suffixes):
+		if suffixes is None:
+			self._extension = []
+		else:
+			self._extension = list(suffixes)
+
+	def extension_is_correct(self):
+		return self._path.suffixes == self.extension
+
+	def extension_to_str(self):
+		ext_str = ""
+
+		for suffix in self.extension:
+			ext_str += suffix
+
+		return ext_str
+
+	def get_file_name(self, with_ext):
+		if not path.is_file():
+			return ""
+
+		file_name = self.path.name
+
+		if not with_ext:
+			ext_index = file_name.index(path.suffixes[0])
+			file_name = file_name[:ext_index]
+
+		return file_name
+
+	@property
+	def path(self):
+		"""
+		The path that this object checks
+		"""
+		return self._path
+
+	@path.setter
+	def path(self, a_path):
+		self._path = a_path
+
+	def path_exists(self):
+		return self._path.exists()
+
+	def path_is_dir(self):
+		return self._path.is_dir()
+
+	def path_is_file(self):
+		return self._path.is_file()
+
+	def remove_suffix(self, suffix):
+		self.extension.remove(suffix)
