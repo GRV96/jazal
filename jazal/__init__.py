c69f98c55c220e10b7e0366657ceb583c71da15d
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-07-03 20:47:14 -0400

    Tests for ExtensionPossessor

--- jazal/__init__.py
+++ jazal/__init__.py
@@ -1,3 +1,4 @@
+from .extension_possessor import ExtensionPossessor
 from .missing_path_arg_warner import MissingPathArgWarner
 from .path_checker import PathChecker
 from .path_util import *
---
+++ tests/exten_possessor_tests.py
@@ -0,0 +1,32 @@
+import pytest
+from jazal import ExtensionPossessor
+
+
+def test_init_none():
+    ep = ExtensionPossessor(None)
+    assert ep.extension == ()
+    assert ep.extension_to_str() == ""
+
+
+def test_init_list_one_suffix():
+    ep = ExtensionPossessor([".docx"])
+    assert ep.extension == (".docx",)
+    assert ep.extension_to_str() == ".docx"
+
+
+def test_init_tuple_one_suffix():
+    ep = ExtensionPossessor((".docx",))
+    assert ep.extension == (".docx",)
+    assert ep.extension_to_str() == ".docx"
+
+
+def test_init_list_two_suffixes():
+    ep = ExtensionPossessor([".tar", ".gz"])
+    assert ep.extension == (".tar", ".gz")
+    assert ep.extension_to_str() == ".tar.gz"
+
+
+def test_init_tuple_two_suffixes():
+    ep = ExtensionPossessor((".tar", ".gz"))
+    assert ep.extension == (".tar", ".gz")
+    assert ep.extension_to_str() == ".tar.gz"
