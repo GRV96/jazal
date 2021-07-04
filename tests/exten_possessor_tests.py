60d41dfc6c440f7edb30eaf3deded0aa35db639b
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-07-03 21:17:53 -0400

    Suffixes in a variable

--- tests/exten_possessor_tests.py
+++ tests/exten_possessor_tests.py
@@ -3,31 +3,36 @@ from jazal import ExtensionPossessor
 
 
 def test_init_none():
-    ep = ExtensionPossessor(None)
+    suffixes = None
+    ep = ExtensionPossessor(suffixes)
     assert ep.extension == ()
     assert ep.extension_to_str() == ""
 
 
 def test_init_list_one_suffix():
-    ep = ExtensionPossessor([".docx"])
+    suffixes = [".docx"]
+    ep = ExtensionPossessor(suffixes)
     assert ep.extension == (".docx",)
     assert ep.extension_to_str() == ".docx"
 
 
 def test_init_tuple_one_suffix():
-    ep = ExtensionPossessor((".docx",))
+    suffixes = (".docx",)
+    ep = ExtensionPossessor(suffixes)
     assert ep.extension == (".docx",)
     assert ep.extension_to_str() == ".docx"
 
 
 def test_init_list_two_suffixes():
-    ep = ExtensionPossessor([".tar", ".gz"])
+    suffixes = [".tar", ".gz"]
+    ep = ExtensionPossessor(suffixes)
     assert ep.extension == (".tar", ".gz")
     assert ep.extension_to_str() == ".tar.gz"
 
 
 def test_init_tuple_two_suffixes():
-    ep = ExtensionPossessor((".tar", ".gz"))
+    suffixes = (".tar", ".gz")
+    ep = ExtensionPossessor(suffixes)
     assert ep.extension == (".tar", ".gz")
     assert ep.extension_to_str() == ".tar.gz"
 
