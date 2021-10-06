db973cb92a5a2eaacb50d729ee97e1ae307f8f3e
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-10-06 17:28:47 -0400

    License added in setup

--- setup.py
+++ setup.py
@@ -12,24 +12,25 @@ import setuptools
 
 
 with open("README.md", "r", encoding="utf-8") as readme_stream:
-    long_description = readme_stream.read()
+	long_description = readme_stream.read()
 
 setuptools.setup(
-    name = "jazal",
-    version = "1.0.0",
-    author = "Guyllaume Rousseau",
-    description = "Jazal performs certain verifications on filepaths before a function or a script uses them.",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/GRV96/jazal",
-    classifiers = [
+	name = "jazal",
+	version = "1.0.0",
+	author = "Guyllaume Rousseau",
+	description = "Jazal performs certain verifications on filepaths before a function or a script uses them.",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/GRV96/jazal",
+	classifiers = [
 		"Development Status :: 5 - Production/Stable",
 		"Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3",
 		"Topic :: Software Development :: Libraries :: Python Modules",
 		"Topic :: Utilities"
-    ],
-    packages = setuptools.find_packages()
+	],
+	packages = setuptools.find_packages(),
+	license = "MIT"
 )
